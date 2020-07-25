75229e9ec9d0967c9c8749e23aee8b9bf8b08e02
adityauser <adityauser225@gmail.com>
2020-07-25 18:40:28 +0530

    Updated parellel

--- UCBvsES/run_parallel.py
+++ UCBvsES/run_parallel.py
@@ -22,27 +22,7 @@ processes = (
     "UCBvsES_update.py --mutation_mag 0.1 --arms 10 --hidden 64 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
     "UCBvsES_update.py --mutation_mag 0.1 --arms 10 --hidden 64 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
     "UCBvsES_update.py --mutation_mag 0.1 --arms 10 --hidden 128 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.1 --arms 10 --hidden 128 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 8 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 8 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 16 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 16 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 32 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 32 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 64 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 64 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 128 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 128 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v1",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 8 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 8 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 16 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 16 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 32 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 32 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 64 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 64 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 128 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2",
-    "UCBvsES_update.py --mutation_mag 0.01 --arms 10 --hidden 128 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2"
+    "UCBvsES_update.py --mutation_mag 0.1 --arms 10 --hidden 128 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 50 --name v2"
 )
 
                                              
