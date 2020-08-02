564e5366eac078caa07ab75b3e623c61a55f899d
adityauser <adityauser225@gmail.com>
2020-08-03 00:37:46 +0530

    Reset Env 50

---
+++ UCBvsES/run_parallel3.py
@@ -0,0 +1,55 @@
+import os                                                                       
+from multiprocessing import Pool
+
+
+processes = (
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 8 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 8 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 16 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 16 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 32 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 32 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 64 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 64 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 128 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 128 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 8 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 8 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 16 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 16 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 32 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 32 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 64 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 64 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 128 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.1 --arms 10 --hidden 128 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 8 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 8 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 16 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 16 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 32 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 32 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 64 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 64 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 128 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 128 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v1",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 8 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 8 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 16 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 16 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 32 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 32 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 64 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 64 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 128 --noise --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2",
+    "UCBvsES.py --mutation_mag 0.01 --arms 10 --hidden 128 --save_result --layers 1 --max_gen 200 --trials 100 --pop_size 100 --name resetEnv50_v2"
+)
+
+                                             
+                                                  
+def run_process(process):                                                             
+    os.system('python {}'.format(process))                                       
+                                                                                                                                                      
+pool = Pool(processes=80)
+pool.map(run_process, processes) 
+
